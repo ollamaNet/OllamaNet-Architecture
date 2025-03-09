7fc093de71b866ae768bf93b08793ef3746019ae
Ibrahim Ahmed <aebrahem743@gmail.com>
2025-03-09 19:34:25 +0200

    new ollama host

--- Ollama_Component/Program.cs
+++ Ollama_Component/Program.cs
@@ -65,7 +65,7 @@ public class Program
         builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
         // Add Ollama API client and Semantic Kernel configuration
-        builder.Services.AddScoped<IOllamaApiClient>(_ => new OllamaApiClient("https://69c3-34-125-142-214.ngrok-free.app"));
+        builder.Services.AddScoped<IOllamaApiClient>(_ => new OllamaApiClient("https://4a9a-34-126-150-3.ngrok-free.app"));
         builder.Services.AddScoped<IOllamaConnector, OllamaConnector>();
         builder.Services.AddScoped<ChatHistory>();
         builder.Services.AddScoped<ChatHistoryManager>();
